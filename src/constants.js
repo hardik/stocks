29733f8b87a24327621d4318a4bc15e86f35a671
Hardik Jagda <hardik2633@gmail.com>
2016-12-09 18:29:22 +0530

    Add PubNub keys to constants.

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const PUBNUB_KEYS = {
+    PUBLISH: 'pub-c-91381dff-8b0b-4f9d-a0c8-5f6f55f56f0a',
+    SUBSCRIBE: 'sub-c-58eaf3d0-baff-11e6-8036-0619f8945a4f'
+};
+
+export const PUBNUB_KEYS_H = {
+    PUBLISH: 'pub-c-248535b7-fe96-4d73-a362-6bc2a8a08d71',
+    SUBSCRIBE: 'sub-c-799ec238-bc98-11e6-963b-0619f8945a4f'
+};
+
