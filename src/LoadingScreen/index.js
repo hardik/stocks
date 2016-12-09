b17309bf0e2de429940148a06e4de9040c567c8a
Hardik Jagda <hardik2633@gmail.com>
2016-12-09 15:01:20 +0530

    Create LoadingScreen component.

---
+++ src/LoadingScreen/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LoadingScreen extends Component {
+    render() {
+        return (
+            <div style={{margin: '40px'}}>
+                Listening on channel "demo"
+            </div>
+        )
+    }
+}
+
+export default LoadingScreen;
