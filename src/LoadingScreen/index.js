3855728b6fec37f3237dd47a789d901b31a6d023
Hardik Jagda <hardik2633@gmail.com>
2016-12-09 15:08:42 +0530

    Add spinner and update LoadingScreen styles.
    - Create a new css file for LoadingScreen.

--- src/LoadingScreen/index.js
+++ src/LoadingScreen/index.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import './LoadingScreen.css';
 
 class LoadingScreen extends Component {
     render() {
         return (
-            <div style={{margin: '40px'}}>
-                Listening on channel "demo"
+            <div className="loading">
+                <div className="spinner">
+                    <div className="bounce1"></div>
+                    <div className="bounce2"></div>
+                    <div className="bounce3"></div>
+                </div>
+
+                <div className="loading-text">Listening on channel <b>stocks</b></div>
             </div>
         )
     }
